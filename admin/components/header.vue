a2accec3de975409e6fbad42ffb59d331d98e907
Your Name <you@example.com>
2020-07-09 02:17:29 +0200

    remove search from header'

--- admin/components/header.vue
+++ admin/components/header.vue
@@ -31,18 +31,6 @@
     <!-- Navbar Header -->
     <nav class="navbar navbar-header navbar-expand-lg">
       <div class="container-fluid">
-        <div class="collapse" id="search-nav">
-          <form class="navbar-left navbar-form nav-search mr-md-3">
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <button type="submit" class="btn btn-search pr-1">
-                  <i class="fa fa-search search-icon"></i>
-                </button>
-              </div>
-              <input type="text" placeholder="Search ..." class="form-control" />
-            </div>
-          </form>
-        </div>
         <ul class="navbar-nav topbar-nav ml-md-auto align-items-center">
           <li class="nav-item toggle-nav-search hidden-caret">
             <a
